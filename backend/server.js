5b10ea09391faa802ea8767f456516829239768d
Rishav7102 <rishavranjan7102@gmail.com>
2024-06-30 22:07:50 +0530

    adding cors

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
+const cors=require("cors");
 const express = require('express')
 const dbConnect = require('./dbConnect')
 const app = express()
 app.use(express.json())
+app.use(cors())
 const path = require('path')
 const userRoute = require('./routes/usersRoute')
 const transactionsRoute = require('./routes/transactionsRoute')
@@ -10,14 +12,14 @@ app.use('/api/transactions/' , transactionsRoute)
 
 const port =process.env.PORT || 4000
 
-if(process.env.NODE_ENV === 'production')
-{
-     app.use('/' , express.static('client/build'))
+// if(process.env.NODE_ENV === 'production')
+// {
+//      app.use('/' , express.static('client/build'))
 
-     app.get('*' , (req, res)=>{
-         res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
-     })
-}
+//      app.get('*' , (req, res)=>{
+//          res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+//      })
+// }
 
 
 app.listen(port,()=>console.log('Node JS server started!'));
